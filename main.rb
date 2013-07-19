424a4f9649ca93ea05581859462a567e3c1fbd3a
Wilson <cewilson19@gmail.com>
2013-07-19 14:46:15 -0700

    intial commit of folders

---
+++ main.rb
@@ -0,0 +1,30 @@
+require 'rubygems'
+require 'sinatra'
+
+set :sessions, true
+
+get '/home' do
+	erb :home
+end
+
+post '/name_input' do
+	puts params['username']
+	redirect '/inline'
+
+end
+
+get '/inline' do
+	"Hi, directly from the action!"
+end
+
+get '/template' do
+	erb :mytemplate
+end
+
+get '/nested_template' do 
+	erb :"/user/profile"
+end
+
+get '/nothere' do
+	redirect '/inline'
+end
